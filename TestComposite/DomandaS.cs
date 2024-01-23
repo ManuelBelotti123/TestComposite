09e439745bebda0ce6cb8b36c73a2fbb259e8028
belotti.20181 <belotti.20181@E2-9-PC01>
2024-01-23 12:42:42 +0100

    Classe DomandaS eredita Domanda

---
+++ TestComposite/DomandaS.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TestComposite
+{
+    internal class DomandaS : Domanda
+    {
+        //costruttori
+        public DomandaS(string testo, List<Risposta> risposte) : base(testo, risposte)
+        {
+            Testo = testo;
+            Risposte = risposte;
+        }
+        public DomandaS()
+        {
+            Testo = "";
+            Risposte = new List<Risposta>();
+        }
+        public DomandaS(Domanda d)
+        {
+            Testo = d.Testo;
+            Risposte = d.Risposte;
+        }
+
+        //Equals
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as DomandaVF);
+        }
+        public bool Equals(DomandaVF d)
+        {
+            return Testo == d.Testo &&
+                Risposte == d.Risposte;
+        }
+
+        //GetHashCode
+        public override int GetHashCode()
+        {
+            return Testo.GetHashCode() ^
+                Risposte.GetHashCode();
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            return Testo + ";" + Risposte;
+        }
+
+        //metodi add, remove, getChild ereditati
+
+        //metodo CalcolaPunteggio
+        public override int CalcolaPunteggio()
+        {
+            //da aggiornare
+            return 2;
+        }
+    }
+}
