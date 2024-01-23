df21ee9316dc45a5a131d6e0c7f27e47a5d48774
belotti.20181 <belotti.20181@E2-9-PC01>
2024-01-23 12:45:05 +0100

    Classe DomandaM eredita Domanda

---
+++ TestComposite/DomandaM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TestComposite
+{
+    internal class DomandaM : Domanda
+    {
+        //costruttori
+        public DomandaM(string testo, List<Risposta> risposte) : base(testo, risposte)
+        {
+            Testo = testo;
+            Risposte = risposte;
+        }
+        public DomandaM()
+        {
+            Testo = "";
+            Risposte = new List<Risposta>();
+        }
+        public DomandaM(Domanda d)
+        {
+            Testo = d.Testo;
+            Risposte = d.Risposte;
+        }
+
+        //Equals
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as DomandaM);
+        }
+        public bool Equals(DomandaM d)
+        {
+            return Testo == d.Testo &&
+                Risposte == d.Risposte;
+        }
+
+        //GetHashCode
+        public override int GetHashCode()
+        {
+            return Testo.GetHashCode() ^
+                Risposte.GetHashCode();
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            return Testo + ";" + Risposte;
+        }
+
+        //metodi add, remove, getChild ereditati
+
+        //metodo CalcolaPunteggio
+        public override int CalcolaPunteggio()
+        {
+            //da aggiornare
+            return 2;
+        }
+    }
+}
